4a9d60e62935bf61519a5bc260c8de42d3e4d213
John Kozak <jk@thameslighter.net>
2020-02-01 14:21:19 +0000

    reorganise into single file

--- mailimp.py
+++ mailimp.py
@@ -9,10 +9,10 @@ import os,pwd
 import subprocess
 import smtplib
 import email,email.utils
-import syslog
 import shutil
-
-syslog.openlog('mailimp')
+import configparser
+import hashlib
+import gzip
 
 def log(txt):
     sys.stderr.write(txt)
@@ -23,12 +23,30 @@ def set_log(_log):
     global log
     log = _log
 
+def chk_out(cmd):
+    return subprocess.check_output(cmd).decode('utf-8').strip()
+
+def load_config(conf_file='config.ini'):
+    c = configparser.ConfigParser(allow_no_value=True)
+    c.read(conf_file)
+    try:
+        n = c['network']
+    except KeyError:
+        n = {}
+    return {
+        'members':  set(c['members'].keys()),
+        'smtphost': n.get('smtphost','localhost'),
+        'domain':   n.get('domain',chk_out('dnsdomainname'))
+    }
+
+
 def procmail(name=None,
              domain=None,
              smtphost='localhost',
              members=[],
              text=None,
-             smtp_connection=None):
+             smtp_connection=None,
+             home_dir=None):
 
     try:
         # validate parms
@@ -37,10 +55,12 @@ def procmail(name=None,
         if name is None:
             name = pwd.getpwuid(os.getuid())[0] # name of mailing list
         if domain is None:
-            domain = subprocess.check_output('dnsdomainname',encoding='utf-8').strip()
+            domain = chk_out('dnsdomainname')
             if not domain:
                 log("no domain specified")
                 return
+        if home_dir is None:
+            home_dir = pwd.getpwuid(os.getuid())[5]
         if smtphost is None:
             smtphost = 'localhost'
             members = [email.utils.parseaddr(m)[1] for m in members]
@@ -60,10 +80,17 @@ def procmail(name=None,
             source = "%s via %s"%(auth_nv[0],source)
         msg['Reply-To'] = msg['From'] = source
 
+        # stash message
+        hasher = hashlib.sha1()
+        hasher.update(text.encode('utf-8'))
+        h = hasher.hexdigest()
+        with gzip.open(os.path.join(home_dir,'received','%s.gz'%(h,)),'wb') as wzf:
+            wzf.write(text.encode('utf-8'))
+        log("saved msg %s from %s as %s"%(msg['Message-ID'],author,h))
+
         # check author is a list member
         if author not in members:
             log("%s is not a member"%(author,))
-            # +++ stash message somewhere +++
             return
 
         # send messages out
@@ -73,7 +100,7 @@ def procmail(name=None,
             if m!=author:
                 msg['Sender'] = author
                 msg['To']     = m
-                log("send msg %s to %s"%(msg['Message-Id'],m))
+                log("send msg %s to %s"%(msg['Message-ID'],m))
                 try:
                     smtp_connection.send_message(msg)
                 except:
@@ -83,30 +110,68 @@ def procmail(name=None,
         if smtp_connection:
             smtp_connection.quit()
 
-def new_list(name):
+def _sanity_check(name,home_dir=None):
+    if home_dir is None:
+        home_dir = pwd.getpwnam(name)[5]
+    # +++ this should not be called on a running system +++
+    # +++  only as part of set up +++
+    # +++ fail if ~name/config.ini exists +++
+    # +++ check user name has a valid shell +++
+    # +++ try to deliver mail +++
+    pass
+
+def make_list(name):
     try:
-        subprocess.check_output("useradd -rmU -s /bin/false %s"%(name,))
+        # N.B. must have a valid login shell for .forward to pipe
+        subprocess.check_output("useradd -rmU %s"%(name,),shell=True)
     except:
-        sys.exit("failed to add list user")
+        sys.exit("failed to add list user %s"%(name,))
     home_dir = pwd.getpwnam(name)[5]
     # +++ maybe: subprocess.check_output("rm -rf %s/*"%(home_dir,))
-    open(os.path.join(home_dir,'main.py','w')).write('''\
-#!/usr/bin/env python3
+    open(os.path.join(home_dir,'config.ini'),'w').write('''\
+[members]
+# put members here, one per line, no equals sign
 
-MEMBERS = []
-
-if __name__=='__main__':
-    import mailimp
-    mailimp.procmail(members=MEMBERS)
+[network]
+# smtphost = mailfae
+# domain   = example.org
 
 ''')
-    open(os.path.join(home_dir,'.forward','w')).write('''\
-"|/usr/bin/flock %s/lockfile %s/main.py"
-'''%(home_dir,home_dir) )
-    subprocess.check_output("chown -R %s.%s %s"%(name,name,home_dir))
+    open(os.path.join(home_dir,'.forward'),'w').write('''\
+"|/usr/bin/flock %s/lockfile %s dotforward"
+'''%(home_dir,__file__) )
+    os.mkdir(os.path.join(home_dir,'received'))
+    subprocess.check_output("chown -R %s.%s %s"%(name,name,home_dir),shell=True)
 
-def del_list(name):
+def remove_list(name):
     try:
-        subprocess.check_output("userdel -rf %s"%(name,))
+        subprocess.check_output("userdel -rf %s"%(name,),shell=True)
     except:
         sys.exit("failed to delete list user")
+
+if __name__=='__main__':
+    import syslog
+    syslog.openlog('mailimp')
+    if len(sys.argv)==1:
+        sys.exit("I must write some help")
+    elif sys.argv[1]=='dotforward':
+        uid,cwd = os.getuid(),os.getcwd()
+        if pwd.getpwuid(uid)[5]!=cwd:
+            log("must run in %s's home dir, not %s"%(uid,os.cwd))
+        else:
+            cfg = load_config()
+            procmail(members  = cfg['members'],
+                     smtphost = cfg['smtphost'],
+                     domain   = cfg['domain'])
+    elif sys.argv[1] in ['mk','make'] and len(sys.argv)==3:
+        if os.getuid()!=0:
+            log("must run as root")
+        else:
+            make_list(sys.argv[2])
+    elif sys.argv[1] in ['rm','remove'] and len(sys.argv)==3:
+        if os.getuid()!=0:
+            log("must run as root")
+        else:
+            remove_list(sys.argv[2])
+    else:
+        sys.exit("unknown cmd: %s"%(sys.argv[1],))
--- test_mailimp.py
+++ test_mailimp.py
@@ -3,6 +3,7 @@ import mailimp
 import pytest
 import email,email.utils
 import copy
+import tempfile,os
 
 @pytest.fixture
 def smtp_connection():
@@ -15,6 +16,16 @@ def smtp_connection():
         def quit(self):
             self.is_open = False
     return SmtpConn()
+
+tmpdirs = []
+
+@pytest.fixture
+def home_dir():
+    d  = tempfile.TemporaryDirectory()
+    dn = d.name
+    os.mkdir(os.path.join(dn,'received'))
+    tmpdirs.append(d)          # keep around till program exit
+    return dn
 
 class Log:
     def __init__(self):
@@ -47,6 +58,20 @@ def test_it_works_the_way_I_think_it_does_2(smtp_connection):
     assert len(smtp_connection.msgs)==0,"get a new connection each time"
     assert len(log.msgs)==0,"get a new log object each time"
 
+def test_chk_out():
+    assert mailimp.chk_out(['echo','hi'])=='hi'
+
+def test_load_config(tmpdir):
+    fn = os.path.join(tmpdir,'config.ini')
+    open(fn,'w').write('''\
+[members]
+a@b.com
+b@c.com
+''')
+    cfg = mailimp.load_config(fn)
+    assert cfg['members']=={'a@b.com','b@c.com'}
+    assert cfg['smtphost']=='localhost'
+
 EMAIL_TEXT = '''\
 From: z@y.co
 Message-ID: <abcdef>
@@ -55,31 +80,33 @@ To: imptest@i.co
 here is the news
 '''
 
-def test_ignore_non_member(smtp_connection):
+def test_ignore_non_member(smtp_connection,home_dir):
     mailimp.procmail(name='imptest',
                      domain='i.co',
                      smtphost='localhost',
                      members=['a@b.co'],
                      text=EMAIL_TEXT,
-                     smtp_connection=smtp_connection)
+                     smtp_connection=smtp_connection,
+                     home_dir=home_dir)
     assert len(smtp_connection.msgs)==0
     assert not smtp_connection.is_open
-    assert len(log.msgs)==1
-    assert log.msgs[0]=="z@y.co is not a member"
+    assert len(log.msgs)==2
+    assert log.msgs[1]=="z@y.co is not a member"
 
-def test_send_one(smtp_connection):
+def test_send_one(smtp_connection,home_dir):
     mailimp.procmail(name='imptest',
                      domain='i.co',
                      smtphost='localhost',
                      members=['a@b.co','z@y.co'],
                      text=EMAIL_TEXT,
-                     smtp_connection=smtp_connection)
+                     smtp_connection=smtp_connection,
+                     home_dir=home_dir)
     assert len(smtp_connection.msgs)==1
     assert not smtp_connection.is_open
-    assert len(log.msgs)==1
-    assert log.msgs[0]=="send msg <abcdef> to a@b.co"
+    assert len(log.msgs)==2
+    assert log.msgs[1]=="send msg <abcdef> to a@b.co"
 
-def test_send_one_crlf(smtp_connection):
+def test_send_one_crlf(smtp_connection,home_dir):
     email_text = EMAIL_TEXT.replace('\n','\r\n')
     assert EMAIL_TEXT!=email_text
     mailimp.procmail(name='imptest',
@@ -87,12 +114,13 @@ def test_send_one_crlf(smtp_connection):
                      smtphost='localhost',
                      members=['a@b.co','z@y.co'],
                      text=email_text,
-                     smtp_connection=smtp_connection)
+                     smtp_connection=smtp_connection,
+                     home_dir=home_dir)
     assert len(smtp_connection.msgs)==1
     assert smtp_connection.msgs[0]['To']=='a@b.co'
     assert not smtp_connection.is_open
-    assert len(log.msgs)==1
-    assert log.msgs[0]=="send msg <abcdef> to a@b.co"
+    assert len(log.msgs)==2
+    assert log.msgs[1]=="send msg <abcdef> to a@b.co"
 
 EMAIL_TEXT_FANCY = '''\
 From: Mr Z <z@y.co>
@@ -102,31 +130,33 @@ To: ImpTest <imptest@i.co>
 here is the news
 '''
 
-def test_send_fancy_email_address(smtp_connection):
+def test_send_fancy_email_address(smtp_connection,home_dir):
     mailimp.procmail(name='imptest',
                      domain='i.co',
                      smtphost='localhost',
                      members=['a@b.co','z@y.co'],
                      text=EMAIL_TEXT_FANCY,
-                     smtp_connection=smtp_connection)
+                     smtp_connection=smtp_connection,
+                     home_dir=home_dir)
     assert len(smtp_connection.msgs)==1
     assert smtp_connection.msgs[0]['To']=='a@b.co'
     assert not smtp_connection.is_open
-    assert len(log.msgs)==1
-    assert log.msgs[0]=="send msg <abcdef> to a@b.co"
+    assert len(log.msgs)==2
+    assert log.msgs[1]=="send msg <abcdef> to a@b.co"
 
-def test_send_two(smtp_connection):
+def test_send_two(smtp_connection,home_dir):
     mailimp.procmail(name='imptest',
                      domain='i.co',
                      smtphost='localhost',
                      members=['a@b.co','b@c.co','z@y.co'],
                      text=EMAIL_TEXT,
-                     smtp_connection=smtp_connection)
+                     smtp_connection=smtp_connection,
+                     home_dir=home_dir)
     assert len(smtp_connection.msgs)==2
     assert smtp_connection.msgs[0]['To']=='a@b.co'
     assert smtp_connection.msgs[1]['To']=='b@c.co'
     assert not smtp_connection.is_open
-    assert len(log.msgs)==2
+    assert len(log.msgs)==3
 
 EMAIL_TEXT_MANY_HEADERS = '''\
 From: z@y.co
@@ -138,13 +168,14 @@ Bcc: l@m.co
 here is the news
 '''
 
-def test_sent_headers(smtp_connection):
+def test_sent_headers(smtp_connection,home_dir):
     mailimp.procmail(name='imptest',
                      domain='i.co',
                      smtphost='localhost',
                      members=['a@b.co','z@y.co'],
                      text=EMAIL_TEXT_MANY_HEADERS,
-                     smtp_connection=smtp_connection)
+                     smtp_connection=smtp_connection,
+                     home_dir=home_dir)
     assert len(smtp_connection.msgs)==1
     assert not smtp_connection.is_open
     msg = smtp_connection.msgs[0]
@@ -155,8 +186,8 @@ def test_sent_headers(smtp_connection):
     assert not msg['Cc']
     assert not msg['Bcc']
     assert email.utils.parseaddr(msg['Sender'])[1]=='z@y.co'
-    assert len(log.msgs)==1
-    assert log.msgs[0]=="send msg <abcdef> to a@b.co"
+    assert len(log.msgs)==2
+    assert log.msgs[1]=="send msg <abcdef> to a@b.co"
 
 EMAIL_TEXT_MANY_HEADERS_FANCY = '''\
 From: Mr Z. <z@y.co>
@@ -168,13 +199,14 @@ Bcc: l@m.co
 here is the news
 '''
 
-def test_sent_headers_fancy(smtp_connection):
+def test_sent_headers_fancy(smtp_connection,home_dir):
     mailimp.procmail(name='imptest',
                      domain='i.co',
                      smtphost='localhost',
                      members=['a@b.co','z@y.co'],
                      text=EMAIL_TEXT_MANY_HEADERS_FANCY,
-                     smtp_connection=smtp_connection)
+                     smtp_connection=smtp_connection,
+                     home_dir=home_dir)
     assert len(smtp_connection.msgs)==1
     assert not smtp_connection.is_open
     msg = smtp_connection.msgs[0]
@@ -186,5 +218,26 @@ def test_sent_headers_fancy(smtp_connection):
     assert not msg['Bcc']
     assert email.utils.parseaddr(msg['Sender'])[1]=='z@y.co'
     assert msg['Message-ID']=='<abcdef>'
-    assert len(log.msgs)==1
-    assert log.msgs[0]=="send msg <abcdef> to a@b.co"
+    assert len(log.msgs)==2
+    assert log.msgs[1]=="send msg <abcdef> to a@b.co"
+
+def test_config_parsing():
+    f,fname = tempfile.mkstemp()
+    os.write(f,b'''\
+[members]
+j_k_234@example.net
+jk@example.net
+jk.ladee@example.com
+
+[network]
+smtphost = mailbot
+domain   = example.com
+''')
+    os.close(f)
+    try:
+        cfg = mailimp.load_config(fname)
+        assert cfg['members']=={'j_k_234@example.net','jk@example.net','jk.ladee@example.com'}
+        assert cfg['smtphost']=='mailbot'
+        assert cfg['domain']=='example.com'
+    finally:
+        os.remove(fname)
